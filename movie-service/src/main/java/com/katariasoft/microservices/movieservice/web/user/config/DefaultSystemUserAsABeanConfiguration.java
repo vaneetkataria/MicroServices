791aa5cf0d53d79121246d3397e8d60ee102dfd3
vaneet <vaneetkataria54@gmail.com>
2018-08-06 18:45:53 +0530

    Added a comment .

--- movie-service/src/main/java/com/katariasoft/microservices/movieservice/web/user/config/DefaultSystemUserAsABeanConfiguration.java
+++ movie-service/src/main/java/com/katariasoft/microservices/movieservice/web/user/config/DefaultSystemUserAsABeanConfiguration.java
@@ -3,6 +3,7 @@ package com.katariasoft.microservices.movieservice.web.user.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+//All property sources can be configured here for which beans are going to be defined one by one.
 //@Configuration
 public class DefaultSystemUserAsABeanConfiguration {
 
