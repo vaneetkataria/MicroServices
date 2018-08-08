3b81fb31cf120b90737c5e93be777cc9dafa55ac
vaneet <vaneetkataria54@gmail.com>
2018-08-08 19:10:23 +0530

    added eureka server

---
+++ spring-cloud-eureka-naming-server/src/test/java/com/katariasoft/microservices/springcloudeurekanamingserver/SpringCloudEurekaNamingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.katariasoft.microservices.springcloudeurekanamingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudEurekaNamingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
