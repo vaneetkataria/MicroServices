e077eb2902420a672ada7389b2a9de8f224a7771
vaneet <vaneetkataria54@gmail.com>
2018-09-14 19:18:07 +0530

    Dockerised application

--- songs-service/src/main/java/com/katariasoft/microservices/songsservice/web/songs/controller/SongsController.java
+++ songs-service/src/main/java/com/katariasoft/microservices/songsservice/web/songs/controller/SongsController.java
@@ -1,7 +1,5 @@
 package com.katariasoft.microservices.songsservice.web.songs.controller;
 
-import javax.ws.rs.HeaderParam;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.katariasoft.microservices.songsservice.web.songs.beans.DefaultSong;
 import com.katariasoft.microservices.songsservice.web.songs.beans.Song;
 
@@ -45,7 +42,7 @@ public class SongsController {
 	@PostMapping(path = "/songs", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<Song> addSong(@RequestParam(name = "singer", required = false) String singer,
 			@RequestParam(name = "lyricist", required = false) String lyricist,
-			@HeaderParam(value = "showParams") boolean showParams, @RequestBody(required = false) Song song) {
+			@RequestBody(required = false) Song song) {
 		song.setPort(port);
 		return ResponseEntity.ok().body(song);
 	}
@@ -53,7 +50,7 @@ public class SongsController {
 	@PutMapping(path = "/songs", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<Song> putSong(@RequestParam(name = "singer", required = false) String singer,
 			@RequestParam(name = "lyricist", required = false) String lyricist,
-			@HeaderParam(value = "showParams") boolean showParams, @RequestBody(required = false) Song song) {
+			@RequestBody(required = false) Song song) {
 		song.setPort(port);
 		return ResponseEntity.ok().body(song);
 	}
